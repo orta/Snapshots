512b1efc17ca8b00544387e9b35235693cef6e76
Orta Therox <orta.therox@gmail.com>
2016-03-09 16:58:20 -0500

    Add more log tests

--- Testing/SnapshotsTests/SnapshotsTestsTests/SnapshotsTestsTests.m
+++ Testing/SnapshotsTests/SnapshotsTestsTests/SnapshotsTestsTests.m
@@ -14,10 +14,8 @@
 
 @implementation SnapshotsTestsTests
 
-- (void)testExample {
-    ORLogReader *reader = [[ORLogReader alloc] init];
-
-    NSString *logPath = [[NSBundle mainBundle] pathForResource:@"MultiSnapshotsPerTest" ofType:@".log"];
+- (void)runLog:(NSString *)string {
+    NSString *logPath = [[NSBundle mainBundle] pathForResource:string ofType:@".log"];
     NSString *log = [NSString stringWithContentsOfFile:logPath encoding:NSUTF8StringEncoding error:nil];
     for (NSString *line in [log componentsSeparatedByString:@"\n"]) {
         ORFakeLog *log = [[ORFakeLog alloc] init];
@@ -26,8 +24,32 @@
         [[NSNotificationCenter defaultCenter] postNotification:notification];
     }
 
+}
+
+- (void)testKnownSnapshots {
+    ORLogReader *reader = [[ORLogReader alloc] init];
+
+    [self runLog:@"MultiSnapshotsPerTest"];
+
     XCTAssert(reader.ksdiffCommands.count == 8, @"Expected 8 diff commands");
     XCTAssert(reader.uniqueDiffCommands.count == 8, @"Expected 8 unique diff commands");
+}
+
+- (void)testNewSyntax {
+    ORLogReader *reader = [[ORLogReader alloc] init];
+
+    [self runLog:@"QuickNewSnapshot"];
+
+    XCTAssert(reader.hasNewSnapshots, @"Expected to have snapshots");
+    XCTAssert(reader.uniqueDiffCommands.count == 1, @"Expected 1 diff commands");
+}
+
+- (void)testJustRecording {
+    ORLogReader *reader = [[ORLogReader alloc] init];
+
+    [self runLog:@"QuickJustNewSnapshot"];
+
+    XCTAssert(reader.hasNewSnapshots, @"Expected to have snapshots");
 }
 
 
